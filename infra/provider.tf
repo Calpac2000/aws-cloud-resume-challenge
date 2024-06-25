5f1413977f99d07c658fef692633d015d38a65e4
Calvin Seok <calvinseok@gmail.com>
2024-06-25 09:25:51 -0700

    IAC commit 1

---
+++ infra/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_providers {
+        aws = {
+            version =">= 4.9.0"
+            source = "hashicorp/aws"
+        }
+    }
+
+}
+provider "aws" {
+    
+    access_key ="AKIAT7KKKFJOQN2RQCK3"
+    secret_key = "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"
+    region = "us-east-1"
+
+}
